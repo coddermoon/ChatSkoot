64d73ca3c55259ef23298aa859048a6b17015cb9
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-12-30 12:44:04 +0600

    logout button added

--- client/Components/TopNav.tsx
+++ client/Components/TopNav.tsx
@@ -1,35 +1,44 @@
 import Image from 'next/image';
 import React from 'react';
-
-const TopNav = ({selectedUser}:
+import { FaPowerOff } from 'react-icons/fa'
+const TopNav = ({ selectedUser }:
     {
         selectedUser: any;
     }) => {
 
-        const {name}= selectedUser
+    const { name } = selectedUser
 
     return (
         <div className='shadow-2xl z-20 '>
-      <div className="flex justify-between items-center mx-16">
-        <div>
-          <div className='h-20 bg-bgPrimary  gap-5 rounded p-4'>
-            <div className="profile flex gap-2 justify-center items-center overflow-hidden">
-              <Image width={20} height={20}
-                className="w-[50px] rounded-full border-[4px] border-primary"
-                src='/images/avatar.png'
-                alt="a"
-              ></Image>
-              <div>
-                <p className='text-white sm:text-md'>{name}</p>
-                <p className=' text-sm text-secondaryWhite'>Active now</p>
-              </div>
+            <div className="flex justify-between items-center mx-16">
+                <div>
+                    <div className=' bg-bgPrimary  gap-5 rounded p-4'>
+                        <div className="flex items-center justify-between">
+                            <div className="profile flex gap-2 justify-center items-center overflow-hidden">
+                                <Image width={20} height={20}
+                                    className="w-[50px] rounded-full border-[4px] border-primary"
+                                    src='/images/avatar.png'
+                                    alt="a"
+                                ></Image>
+                                <div>
+                                    <p className='text-white sm:text-md'>{name}</p>
+                                    <p className=' text-sm text-secondaryWhite'>Active now</p>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+
+                    </div>
+
+                </div>
+                {/* logout button */}
+                <div className="logout">
+                                <FaPowerOff />
+                            </div>
+
             </div>
-          </div>
-
         </div>
-
-      </div>
-    </div>
     );
 };
 
